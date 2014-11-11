846f5ba12b88534408aac455ce703c81a2010c83
yumashev <a.yumashev@gmail.com>
2014-11-11 21:08:24 +0500

    fix errors

--- includes/user_to_meta.php
+++ includes/user_to_meta.php
@@ -5,23 +5,26 @@ function add_at_js_users_to_meta( $comment_ID, $comment_approved ){
 	if( $comment_approved == 0 )
 		return;
 		
-	$comment = get_comment( $comment_id );
+	$comment = get_comment( $comment_ID );
 	$post = get_post( $comment->comment_post_ID );
 	$user = get_userdata( $post->post_author );
 	
-	$content_for_serach = $post;
+
+	$content_for_serach = $comment->comment_content;
 	//Массив для ID пользователей, которых нужно уведомить
 	$users_ids_for_comment_meta = array();
 	
 	// Разбиваем текст по пробелам на слова и добавляем в массив
 	$content_words = explode(" ", $content_for_serach);
+
 	
 	//Проверем наличия символа @ в начале слова, если есть, то добавляем ид этого пользователя в массив
 	foreach ($content_words as $value) {
 		$rest = substr($value, 0, 1);
 		if ($rest === '@') {			
 			//Добавляем массив с ИД'шниками для уведомления по почте в мету коммента
-			if($user = get_user_by('login', substr($rest, 1))) add_metadata('comment', $comment_ID, 'users_mention_cp', $user->ID, false);
+			$user = get_user_by('login', substr($value, 1));
+			if(isset($user->ID)) add_metadata('comment', $comment_ID, 'users_mention_cp', $user->ID, false);
 		}
 	}
 	
